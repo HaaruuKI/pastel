d8d65c9a4a3676b89fb983e2094e9455db317cbb
AnthonyPy1511 <viceten.antonio.gallegos@gmail.com>
2024-04-15 08:57:10 -0700

    Refactor NavBar widget and update dependencies

--- lib/nav_bar.dart
+++ lib/nav_bar.dart
@@ -1,11 +1,13 @@
+// import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:pastel/feactures/app/constants.dart';
+import 'package:pastel/offline/screens/home/home_screen_guest.dart';
+import 'package:pastel/offline/screens/profile/profile_user_guest.dart';
 import 'package:pastel/feactures/app/pages/home/home_screen.dart';
 import 'package:pastel/feactures/app/pages/order/order_page.dart';
-import 'package:pastel/feactures/app/pages/profile/profile_page.dart';
-import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
+import 'package:pastel/feactures/app/pages/profile/profile_user.dart';
+//Importando las 'Pages' tab.
 
 class NavBar extends StatefulWidget {
   const NavBar({super.key});
@@ -15,86 +17,109 @@ class NavBar extends StatefulWidget {
 }
 
 class _NavBarState extends State<NavBar> {
-  PersistentTabController _controller =
-      PersistentTabController(initialIndex: 0);
+  //Arreglo de objetos.
+  final List<Widget> _widgetOptions = [
+    HomeScreen(),
+    OrderPage(),
+    const ProfieUser(),
+  ];
 
-  Stream<User?> _userStream = FirebaseAuth.instance.authStateChanges();
+  final List<Widget> _widgetOptionsGuest = [
+    HomeScreenGuest(),
+    const ProfileUserGuest(),
+  ];
+
+  //declaracion variable tipo entero para jugar con los indices del arreglo.
+  int _selectedItem = 0;
+  int _selectedItemGuest = 0;
+
+  // Obtén el usuario actual
+  void _cambioWidgetGuest(int index) {
+    setState(() {
+      _selectedItemGuest = index;
+    });
+  }
+
+  void _cambioWidget(int index) {
+    setState(() {
+      _selectedItem = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-      stream: _userStream,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return CircularProgressIndicator();
-        }
-        final user = snapshot.data;
-        return PersistentTabView(
-          context,
-          controller: _controller,
-          screens: _buildScreens(user),
-          items: _navBarsItems(user),
-          confineInSafeArea: true,
-          backgroundColor: Colors.white, // Default is Colors.white.
-          handleAndroidBackButtonPress: true, // Default is true.
-          resizeToAvoidBottomInset:
-              true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
-          stateManagement: true, // Default is true.
-          hideNavigationBarWhenKeyboardShows:
-              true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
-          decoration: NavBarDecoration(
-            borderRadius: BorderRadius.circular(10.0),
-            colorBehindNavBar: Colors.white,
-          ),
-          popAllScreensOnTapOfSelectedTab: true,
-          popActionScreens: PopActionScreensType.all,
-          itemAnimationProperties: const ItemAnimationProperties(
-            // Navigation Bar's items animation properties.
-            duration: Duration(milliseconds: 200),
-            curve: Curves.ease,
-          ),
-          screenTransitionAnimation: const ScreenTransitionAnimation(
-            // Screen transition animation on change of selected tab.
-            animateTabTransition: true,
-            curve: Curves.ease,
-            duration: Duration(milliseconds: 200),
-          ),
-          navBarStyle: NavBarStyle
-              .style7, // Choose the nav bar style with this property.
-        );
-      },
-    );
+    User? user;
+    try {
+      user = FirebaseAuth.instance.currentUser;
+    } on FirebaseException {
+      return Scaffold(
+        body: _widgetOptions.elementAt(_selectedItemGuest),
+        bottomNavigationBar: _bottomNavBar(context),
+      );
+    }
+
+    if (user != null) {
+      return Scaffold(
+        body: _widgetOptions.elementAt(_selectedItem),
+        bottomNavigationBar: _bottomNavBar(context),
+      );
+    } else {
+      return Scaffold(
+        body: _widgetOptionsGuest.elementAt(_selectedItemGuest),
+        bottomNavigationBar: _bottomNavBarGuest(context),
+      );
+    }
   }
 
-  List<Widget> _buildScreens(User? user) {
-    return [
-      HomeScreen(),
-      const OrderPage(),
-      const ProfilePage(),
-    ];
+  Widget _bottomNavBarGuest(BuildContext context) {
+    return BottomNavigationBar(
+        iconSize: 30.0,
+        selectedItemColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.grey,
+        currentIndex: _selectedItemGuest,
+        onTap: _cambioWidgetGuest,
+        showUnselectedLabels: true,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Inicio',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_outlined),
+            label: 'Perfil',
+          ),
+        ]);
   }
 
-  List<PersistentBottomNavBarItem> _navBarsItems(User? user) {
-    return [
-      PersistentBottomNavBarItem(
-        icon: Icon(CupertinoIcons.home, color: CupertinoColors.black),
-        title: ("Home"),
-        activeColorPrimary: yellowPastel,
-        inactiveColorPrimary: CupertinoColors.systemGrey,
-      ),
-      // if (user != null)
-      PersistentBottomNavBarItem(
-        icon: Icon(CupertinoIcons.ticket, color: CupertinoColors.black),
-        title: ("Settings"),
-        activeColorPrimary: yellowPastel,
-        inactiveColorPrimary: CupertinoColors.systemGrey,
-      ),
-      PersistentBottomNavBarItem(
-        icon: Icon(CupertinoIcons.settings, color: CupertinoColors.black),
-        title: ("Settings"),
-        activeColorPrimary: yellowPastel,
-        inactiveColorPrimary: CupertinoColors.black,
-      ),
-    ];
+  Widget _bottomNavBar(BuildContext context) {
+    return BottomNavigationBar(
+        iconSize: 30.0,
+        selectedItemColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.grey,
+        currentIndex: _selectedItem,
+        onTap: _cambioWidget,
+        showUnselectedLabels: true,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Inicio',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.check_circle),
+            label: 'Orden',
+          ),
+          // BottomNavigationBarItem(
+          //   icon: Icon(Icons.bolt_rounded),
+          //   label: 'Favoritos',
+          // ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_outlined),
+            label: 'Perfil',
+          ),
+          // BottomNavigationBarItem(
+          //   icon: Icon(Icons.account_circle_outlined),
+          //   label: 'Pruebas',
+          // ),
+        ]);
   }
 }
